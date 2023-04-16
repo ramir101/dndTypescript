c6794e1c4d8c2fae27d68b0edb83a51c26b80664
Ramir Salcedo <ramir101@gmail.com>
2023-04-16 19:08:06 -0400

    set up done from copy

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// }
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// }
+// interface elevatedEmployee extends Admin, Employee {}
+const e1 = {
+    name: "ramir",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
+//# sourceMappingURL=app.js.map
